ad9cfd45675a1b75af31e2dd90f183b2a6793049
Sternbach-Software <60489781+Sternbach-Software@users.noreply.github.com>
2023-04-23 07:51:00 -0400

    Create ValidParentheses.kt

---
+++ ValidParentheses.kt
@@ -0,0 +1,49 @@
+class Solution {
+
+    /**
+
+    if its a closing one and the last one wasnt its opening one, then invalid. if last was opening, remove last. if opening, add. if none removed, false. if chars remaining, false.
+
+    */
+
+    fun isValid(s: String): Boolean {
+
+        val stack = StringBuilder(s.length) // can also use ArrayDeque
+
+        var atLeastOnePairFound = false
+
+        for(char in s) {
+
+           if(char == '(' || char == '{' || char == '[') stack.append(char) 
+
+            else {
+
+                val last = stack.lastOrNull()
+
+            if(
+
+                (char == ')' && last != '(') ||
+
+                (char == '}' && last != '{') || (char == ']' && last != '[')     
+
+            ) return false else {
+
+                //stack.removeLastOrNull()
+
+                if (stack.isEmpty()) null else stack.deleteCharAt(stack.lastIndex)
+
+                atLeastOnePairFound = true
+
+                }
+
+           }
+
+            }
+
+        return atLeastOnePairFound && stack.isEmpty()
+
+    }
+
+    //public fun <T> MutableList<T>.removeLastOrNull(): T? = if (isEmpty()) null else removeAt(lastIndex)
+
+}
