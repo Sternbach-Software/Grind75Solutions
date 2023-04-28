9f97f62a611a782d0d57ba2ec3c3547302c41ce4
shmueldabomb441 <shmuelsternbach613@gmail.com>
2023-04-28 15:31:10 -0400

    Copy other person's solution

--- Main.kt
+++ Main.kt
@@ -5,6 +5,7 @@ fun main() {
     val list2 = SolutionMergeSortedLists.ListNode(1)
     list2.next = SolutionMergeSortedLists.ListNode(3)
     list2.next!!.next = SolutionMergeSortedLists.ListNode(4)
-    println(SolutionMergeSortedLists().mergeTwoLists(list1, list2))
+    list2.next!!.next!!.next = SolutionMergeSortedLists.ListNode(5)
+    println("Final result: " + SolutionMergeSortedLists().mergeTwoLists(list2, null))
     SolutionValidParentheses().isValid("(){}{[]}")
 }
--- MergeSortedLists.kt
+++ MergeSortedLists.kt
@@ -17,57 +17,82 @@
  */
 
 class SolutionMergeSortedLists {
-    data class ListNode(
+    class ListNode(
         var `val`: Int,
         var next: ListNode? = null
-    )
+    ) {
+        override fun toString(): String {
+            return "ListNode(val = $`val`, next = $next)"
+        }
+    }
 
     fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
         println("mergeTwoLists(list1 = $list1, list2 = $list2)")
-        if (list1 == null)
+        /*if (list1 == null)
             return list2
-        else if (list2 == null) /*only 2*/ return list1
-        var final: ListNode? = null
+        else if (list2 == null) *//*only 2*//* return list1
+        var final: ListNode? = ListNode(0)
+        var next: ListNode? = null
+        final?.next = next
         var i = list1
         var j = list2
         while (i != null || j != null) {
+            println("Loop: \ni = $i, \nj = $j, \nfinal = $final")
             if (i != null) {
                 if (j != null) {
                     //both not null
-                    val iIsLess = i.`val` < j.`val`
-                    if (final == null)
-                        if (iIsLess) {
-                            final = i
-                            final.next = j
-                        } else {
-                            final = j
-                            final.next = i
-                        }
-                    else {
-                        if (iIsLess) {
-                            final.next = i
-                            final.next!!.next = j
-                        } else {
-                            final.next = j
-                            final.next!!.next = i
-                        }
-                        final = final.next!!.next
-                    }
+                    val node =
+                        if (i.`val` < j.`val`) ListNode(i.`val`, j)
+                        else ListNode(j.`val`, i)
+
+                    if (next == null)
+                        next = node
+                    else
+                        next.next = node
                 } else {
                     //only j null
-                    if(final == null) final = i
-                    else final.next = i
-                    final = final.next
+                    if (next == null) next = i
+                    else next.next = i
                 }
-            } else { /*j cannot be null because then they would both be null, and the loop wouldn't have ran*/
+            } else { *//*j cannot be null because then they would both be null, and the loop wouldn't have ran*//*
                 //only i null
-                if(final == null) final = j
-                else final.next = j
-                final = final!!.next
+                if (next == null) next = j
+                else next.next = j
             }
+            println("Before reset:")
+            println("final: $final")
+            println("i: $i")
+            println("j: $j")
+
+            next = next?.next
             i = i?.next
             j = j?.next
         }
-        return final
+        return final*/
+        val result = ListNode(0)
+        var current = result
+
+        var node1 = list1
+        var node2 = list2
+        while (node1 != null || node2 != null) {
+            if (node1 == null) {
+                current.next = node2
+                break
+            }
+            if (node2 == null) {
+                current.next = node1
+                break
+            }
+
+            if (node1.`val` < node2.`val`) {
+                current.next = node1
+                node1 = node1.next
+            } else {
+                current.next = node2
+                node2 = node2.next
+            }
+            current = current.next!!/*compiler can't yet determine nullability of/smart cast 'complex expressions'*/
+        }
+        return result.next
     }
 }
