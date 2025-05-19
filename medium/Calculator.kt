0d7e037b6adb72417544ef67bc2e622c52c8becd
shmueldabomb441 <shmuelsternbach613@gmail.com>
2025-05-18 21:48:27 -0400

    Implement calculator

---
+++ medium/Calculator.kt
@@ -0,0 +1,108 @@
+package medium
+
+import ListNode
+
+class Calculator {
+    fun calculate(s: String): Int {
+        val numbers = hashSetOf('1', '2', '3', '4', '5', '6', '7', '8', '9', '0')
+        val operators = hashSetOf('+', '-', '/', '*')
+
+        val multiplicationQueue = ArrayDeque<ListNode<String>>()
+        val divisionQueue = ArrayDeque<ListNode<String>>()
+        val additionQueue = ArrayDeque<ListNode<String>>()
+        val subtractionQueue = ArrayDeque<ListNode<String>>()
+        var head: ListNode<String>? = null
+        var currentTail: ListNode<String>? = null
+        var currentNum: String? = null
+
+        for (char in s) {
+            println("Iterating $char")
+            if (char in numbers) {
+                if (currentNum == null) {
+                    currentNum = char.toString()
+                    continue
+                } else currentNum += char
+                println("Current num: $currentNum")
+            } else if (char in operators) {
+                //add currentNum and operator, and reset char run
+
+                val operator = ListNode(char.toString())
+                println("Operator: $char, Current num: $currentNum")
+                if (head == null) {
+                    //currentTail is also null
+                    head = ListNode(currentNum!!)
+                    currentTail = operator
+                    head?.next = currentTail
+                    currentTail?.prev = head
+                } else {
+                    val prev = currentTail
+                    currentTail = ListNode(currentNum!!)
+                    prev?.next = currentTail
+                    currentTail?.prev = prev
+                }
+                currentNum = null
+
+                //add operator
+
+                when (char) {
+                    '+' -> additionQueue.addFirst(operator)
+                    '-' -> subtractionQueue.addFirst(operator)
+                    '*' -> multiplicationQueue.addFirst(operator)
+                    '/' -> divisionQueue.addFirst(operator)
+                }
+            }
+        }
+        //add last num
+        val prev = currentTail
+        currentTail = ListNode(currentNum!!)
+        prev?.next = currentTail
+        currentTail?.prev = prev
+
+        //MDAS
+        var node: ListNode<String>? = null
+        for (queue in listOf(
+            multiplicationQueue,
+            divisionQueue,
+            additionQueue,
+            subtractionQueue
+        )) while (queue.isNotEmpty()) {
+            val operator = queue.removeLast()
+            val char = operator.value[0]
+
+            val left = operator.prev
+            val right = operator.next
+            val leftNum = left?.value?.toInt()
+            val rightNum = right?.value?.toInt()
+            if (leftNum == null || rightNum == null) continue
+            val answer = when (char) {
+                '+' -> leftNum + rightNum
+                '-' -> leftNum - rightNum
+                '*' -> leftNum * rightNum
+                '/' -> (leftNum / rightNum).toInt()
+                else -> 0
+            }
+            println("$leftNum $char $rightNum = $answer")
+
+            node = ListNode(answer.toString())
+            val newLeft = left?.prev
+            val newRight = right?.next
+            newLeft?.next = node
+            newRight?.prev = node
+            node.prev = newLeft
+            node.next = newRight
+            println(node.getHead().getHeadString())
+        }
+        return node?.value?.toInt() ?: 0
+    }
+
+
+    companion object {
+        @JvmStatic
+        fun main(array: Array<String>) {
+            println(Calculator().calculate("1+2*3") == 7)
+            println(Calculator().calculate(" 3/2 ") == 1)
+            println(Calculator().calculate(" 3/2*2 ") == 0)
+
+        }
+    }
+}
